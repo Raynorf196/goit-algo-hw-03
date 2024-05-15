595e549a3f3b67a96c138da8fd6f9b50c7280440
Raynorf196 <64966034+Raynorf196@users.noreply.github.com>
2024-05-15 22:38:13 +0300

    hw 3

---
+++ main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import random
+
+
+def get_days_from_today(date):
+    date_convert = datetime.strptime(date, '%Y-%m-%d')
+    current_date = datetime.now()
+    result = current_date - date_convert
+    return result
+
+def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
+        return []
+    numbers = set()
+    while len(numbers) < quantity:
+        numbers.add(random.randint(min, max))
+    return sorted(list(numbers))
