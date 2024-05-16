78bbb21c7f9ab1a131b5de65458ce195b6182d6e
Raynorf196 <64966034+Raynorf196@users.noreply.github.com>
2024-05-16 20:00:46 +0300

    update hw 3

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_days_from_today(date):
     date_convert = datetime.strptime(date, '%Y-%m-%d')
     current_date = datetime.now()
     result = current_date - date_convert
-    return result
+    return result.days
 
 def get_numbers_ticket(min, max, quantity):
     if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
